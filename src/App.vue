02cd36fb33781d84373255eb983a8d2debe1fd31
tim minker <nigel8181@gmail.com>
2022-06-30 12:03:46 -0400

    add static config

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from 'vue'
+import Add from './components/Add.vue'
+
+let pets = ref([])
+let newPet = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
+let view = ref('main')
+
+const addView = () => {
+  view.value = 'add'
+}
+
+const handleCreate = () => {
+  axios.post('https://forneverhome-backend.herokuapp.com/api/pets', newPet.value)
+  .then((response) =>{ pets.value = [...pets.value, response.data]
+  newPet.value = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
+  })
+}
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>For-Never Home</h1>
+    <button @click="addView">Memorialize Your Pet</button>
   </header>
+  <div class="add-form">
+    <Add :newPet="newPet" :handleCreate="handleCreate"/>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style>
 @import './assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
---
+++ src/components/Add.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="handleCreate">
+    <label htmlFor="name">Name</label>
+    <input v-model="newPet.name" type="text"><br/>
+    <label htmlFor="species">Species</label>
+    <input v-model="newPet.species" type="text"><br/>
+    <label htmlFor="image">Image</label>
+    <input v-model="newPet.image" type="text"><br/>
+    <label htmlFor="owner">Owner</label>
+    <input v-model="newPet.owner" type="text"><br/>
+    <label htmlFor="notes">Description</label>
+    <input v-model="newPet.notes" type="text"><br/>
+    <label htmlFor="thoughts">Thoughts and Prayers</label>
+    <input v-model="newPet.thoughts" type="text"><br/>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['handleCreate', 'newPet'],
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
