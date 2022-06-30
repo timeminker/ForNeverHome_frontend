a41b9fb4e4ff10e492a09868ca3440c0044b27a8
tim minker <nigel8181@gmail.com>
2022-06-30 12:41:40 -0400

    testing for heroku

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <script setup lang="ts">
 import {ref} from 'vue'
-// import Add from './components/Add.vue'
+import Add from './components/Add.vue'
 import axios from 'axios'
 
-// let pets = ref([])
-// let newPet = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
+let pets = ref([])
+let newPet = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
 let view = ref('main')
 
 const addView = () => {
   view.value = 'add'
 }
-//
-// const handleCreate = () => {
-//   axios.post('https://forneverhome-backend.herokuapp.com/api/pets', newPet.value)
-//   .then((response) =>{ pets.value = [...pets.value, response.data]
-//   newPet.value = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
-//   })
-// }
+
+onMounted(() => {
+  axios.get('https://forneverhome-backend.herokuapp.com/api/pets')
+  .then((response) =>{ pets.value = response.data
+    console.log(pets)
+  })
+})
+
+const handleCreate = () => {
+  axios.post('https://forneverhome-backend.herokuapp.com/api/pets', newPet.value)
+  .then((response) =>{ pets.value = [...pets.value, response.data]
+  newPet.value = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
+  })
+}
+
+const handleUpdate = (petID) => {
+  axios.put('https://forneverhome-backend.herokuapp.com/api/pets/' + petID, editPet.value)
+  .then((response)=> {
+    editPet.value = ref({name: '', species: '', image: '', owner: '', notes: '', thoughts: ''})
+    pets.value = pets.value.map((pet) => {
+      return pet.id !== response.data.id ? pet : response.data
+    })
+  })
+}
+
+const handleDelete = (petID) => {
+  axios.delete('https://forneverhome-backend.herokuapp.com/api/pets/' + petID)
+  .then((response) => {
+    pets.value = pets.value.filter(pet => pet.id !== petID)
+  })
+}
 
 </script>
 
@@ -26,7 +50,7 @@ const addView = () => {
     <button @click="addView">Memorialize Your Pet</button>
   </header>
   <div class="add-form"> -->
-    <!-- <Add :newPet="newPet" :handleCreate="handleCreate"/> -->
+    <Add :newPet="newPet" :handleCreate="handleCreate"/>
   </div>
 
 </template>
